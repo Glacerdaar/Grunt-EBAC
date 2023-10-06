fa2dc99bd2b09b28165ffb71725fc101ea7fbd5a
Gabriel <glacerdaar@gmail.com>
2023-10-06 11:59:11 -0300

    Projeto finalizado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('form-sort').addEventListener('submit', function(){
+    document.getElementById('form-sort').addEventListener('submit', function(e){
+        e.preventDefault();
         let numeroMaximo = document.getElementById('numeroMaximo').value;
         numeroMaximo = parseInt(numeroMaximo);
 
-        let numeroAleatorio = Math.random() * numeroMaximo;
-        alert(numeroAleatorio);
+        let numeroAleatorio = Math.random() * numeroMaximo + 1;
+        numeroAleatorio = Math.floor(numeroAleatorio)
+        document.getElementById('result-value').innerText = numeroAleatorio
+        document.querySelector('.result').style.display = "block";
     })
 })
